6948ad62458133e99ef813449c1c46667f3363e1
ivanntis <ivanntis@gmail.com>
2019-07-23 00:47:03 -0500

    Se crea grupo y usuario para serverless

--- pulse-inf.tf
+++ pulse-inf.tf
@@ -146,3 +146,31 @@ resource "aws_instance" "pulse-app-ec2" {
         app  = "pulse"
     }
 }
+
+resource "aws_iam_account_password_policy" "strict" {
+    minimum_password_length = 8
+    require_lowercase_characters = true
+    require_numbers = true
+    require_uppercase_characters = true
+    require_symbols = false
+    allow_users_to_change_password = true
+    password_reuse_prevention = 3
+    max_password_age = 180
+ }
+resource "aws_iam_user" "pulse-api-user" {
+    name = "pulse-api"
+}
+resource "aws_iam_group" "pulse-admin-api" {
+  name = "pulse-admin-api"
+}
+resource "aws_iam_group_policy_attachment" "engineering-attach" {
+  group = "${aws_iam_group.pulse-admin-api.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
+}
+
+resource "aws_iam_group_membership" "pulse-api-user-groups" {
+    name    = "pulse-api-user-groups"
+    users   = ["${aws_iam_user.pulse-api-user.name}"]
+    group   = "${aws_iam_group.pulse-admin-api.name}"
+}
